104c89a9b6a42a4e67ab03336b463dfee38477dc
markmead <itsmarkmead@gmail.com>
2020-11-17 17:56:26 +0000

    update controller

--- app/controllers/spina/articles_controller.rb
+++ app/controllers/spina/articles_controller.rb
@@ -1,13 +1,23 @@
 module Spina
-  class ArticlesController < Spina::ApplicationController
+  class ArticlesController < ApplicationController
     def index
       @articles = Spina::Article.live
                                 .published
                                 .order(publish_date: :desc)
+
+      render layout: theme_layout
     end
 
     def show
       @article = Spina::Article.find(params[:id])
+
+      render layout: theme_layout
+    end
+
+    private
+
+    def theme_layout
+      "#{current_theme.name.parameterize.underscore}/application"
     end
   end
 end
